c44abf54baa93d936c508a301ad48aca773e198c
erwaanz <erwaanz@gmail.com>
2019-04-30 08:51:39 +0000

    initial commit

--- html/resources/webapp/controller/algorithms/pal/auto_smoothing.controller.js
+++ html/resources/webapp/controller/algorithms/pal/auto_smoothing.controller.js
@@ -2,8 +2,8 @@ sap.ui.define([
 	"fypforecast/html/base/algorithms/Controller"
 ], function(Controller) {
 	"use strict";
-	return Controller.extend("fypforecast.html.controller.algorithms.pal.auto_smooting", {
-		forcedSelectedAlgorithm: "auto_smooting",
+	return Controller.extend("fypforecast.html.controller.algorithms.pal.auto_smoothing", {
+		forcedSelectedAlgorithm: "auto_smoothing",
 		onPressExecute: function(oEvent) {
 			Controller.prototype.onPressExecute.apply(this, oEvent);
 				this.setVizProperties("result_fit_viz_frame", "result_fit_popover");			
